ec6494386c21b2587614088c256336c777a8fdb2
Mtumishi <blakhite@gmail.com>
2024-04-02 23:12:17 -0400

    April

--- TechShop/payment/views.py
+++ TechShop/payment/views.py
@@ -67,3 +67,63 @@ nonce is generated with the Braintree JavaScript client. You send the token
 to your view with a POST request."""
 
 """4. The payment view receives the token nonce and you use it to generate a transaction using the braintree Python module."""
+
+
+
+#Mpesa Payment Integration
+
+from django.http import HttpResponse
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+from . credentials import MpesaAccessToken, LipanaMpesaPpassword
+
+
+def getAccessToken(request):
+    consumer_key = '77bgGpmlOxlgJu6oEXhEgUgnu0j2WYxA'
+    consumer_secret = 'viM8ejHgtEmtPTHd'
+    api_URL = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
+
+    r = requests.get(api_URL, auth=HTTPBasicAuth(
+        consumer_key, consumer_secret))
+    mpesa_access_token = json.loads(r.text)
+    validated_mpesa_access_token = mpesa_access_token['access_token']
+
+    return HttpResponse(validated_mpesa_access_token)
+
+
+def lipa_na_mpesa_online(request):
+    access_token = MpesaAccessToken.validated_mpesa_access_token
+    api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+    headers = {"Authorization": "Bearer %s" % access_token}
+    request = {
+        "BusinessShortCode": LipanaMpesaPpassword.Business_short_code,
+        "Password": LipanaMpesaPpassword.decode_password,
+        "Timestamp": LipanaMpesaPpassword.lipa_time,
+        "TransactionType": "CustomerPayBillOnline",
+        "Amount": 1,
+        "PartyA": 254707535013,
+        "PartyB": LipanaMpesaPpassword.Business_short_code,
+        "PhoneNumber": 254707535013,
+        "CallBackURL": "https://sandbox.safaricom.co.ke/mpesa/",
+        "AccountReference": "Alex",
+        "TransactionDesc": "Testing stk push"
+    }
+
+    response = requests.post(api_url, json=request, headers=headers)
+    return HttpResponse('success')
+'''
+def pull_transaction(request):
+    access_token = MpesaAccessToken.validated_mpesa_access_token
+    api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+    headers = {"Authorization": "Bearer %s" % access_token}
+    request = {
+        "BusinessShortCode": LipanaMpesaPpassword.Business_short_code,
+        "Timestamp": LipanaMpesaPpassword.lipa_time,
+        "EndDate": LipanaMpesaPpassword.EndDate,
+        "OffSetValue": "0"
+    }
+
+    response = requests.post(api_url, json=request, headers=headers)
+    return HttpResponse('success')
+'''
